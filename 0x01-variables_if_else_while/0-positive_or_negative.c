c925fe7c0683a2df1f7ac4d29e2701017c90b179
Nat-251 <natygizachew@gmail.com>
2022-07-01 16:54:17 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/**                                                                              
+/**
  * main - This is just a main function
  * Return: Nothing is returned
  */
