ad28a8e04270de012c405194c42115d46286ee55
Nat-251 <natygizachew@gmail.com>
2022-07-01 17:45:45 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,17 +4,16 @@
 
 /**
  * main - Task 1 print the last digit of the number stored in the variable n
- *
  *Return: 0
- **/
+*/
 
 int main(void)
 {
         int n;
         char last[] = "Last digit of";
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
         if (n % 10 > 5)
         {
