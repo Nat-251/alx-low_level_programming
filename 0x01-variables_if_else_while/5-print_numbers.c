9a67e629f3c2d070c4cd5aa9db21bb7d0295da8f
Nat-251 <natygizachew@gmail.com>
2022-07-05 00:26:40 -0700

    variables if else while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,19 +3,17 @@
 /**
  *main - prints all single digit numbers of base 10 starting from 0
  *followed by a new line
- *
- *Return: 0 if success
- **/
+  *Return: 0 if success
+*/
 int main(void)
 {
-        char d = '0';
+char d = '0';
 
-        while (d <= '9')
-        {
-        putchar(d);
-        d++;
-        }
-        putchar('\n');
-        return (0);
+while (d <= '9')
+{
+putchar(d);
+d++;
 }
-
+putchar('\n');
+return (0);
+}
