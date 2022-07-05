1aaeed0661f27216c640eb685a4357a5556effb8
Nat-251 <natygizachew@gmail.com>
2022-07-05 04:16:58 -0700

    hotfix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,32 +5,32 @@
 /**
  *main -> print all possible different combinations of two digits
  *
- *Return : always 0
+ *Return : 0 if success
  */
 
 int main(void)
 {
-        int ch;
-        int n;
+int ch;
+int n;
 
-        for (ch = 48; ch<= 57; ch++)
-        {
-                for (n = 49; n <= 57; n++)
-                {
-			if (n > ch)
-                        {
-				putchar(ch);
-                                putchar(n);
-                                if (ch != 56 || n != 57)
-                                {
-					putchar(44);
-                                        putchar(32);
-                                }
-                        }
-                }
-        }
+for (ch = 48; ch<= 57; ch++)
+{
+for (n = 49; n <= 57; n++)
+{
+if (n > ch)
+{
+putchar(ch);
+putchar(n);
+if (ch != 56 || n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
 
-        putchar(10); /* this is an ascii code for new line*/
+putchar(10); /* this is an ascii code for new line*/
 
-        return (0);
+return (0);
 }
