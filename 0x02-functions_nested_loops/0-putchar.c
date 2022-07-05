012ad261e761a52269fd24844aaa3ed621d7405e
Nat-251 <natygizachew@gmail.com>
2022-07-05 12:59:25 -0700

    minor-fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <main.h>
 
 /**
  * main -> Write a program that prints _putchar,
@@ -6,7 +6,18 @@
  *
  * Return: On success 0
 */
-int _putchar(char c)
+int main(void)
 {
+	{
+		_putchar("_");
+		_putchar("p");
+		_putchar("u");
+		_putchar("t");
+		_putchar("c");
+		_putchar("h");
+		_putchar("a");
+		_putchar("r");
+		_putchar("\n");
+	}
 	return (0);
 }
