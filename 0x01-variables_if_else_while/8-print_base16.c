25aacb088aa67176de25bf851fc36444ca43b916
Nat-251 <natygizachew@gmail.com>
2022-07-05 01:03:54 -0700

    variables if else while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,23 +9,22 @@
 
 int main(void)
 {
-        /**
-         *
-         * hexadecimal
-         * 0 1 2 3 4 5 6 7 8 9 a c d e f
-         */
-        char ch;
-        int n;
+/**
+*
+* hexadecimal
+* 0 1 2 3 4 5 6 7 8 9 a c d e f
+*/
+char ch;
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar(10);
 
-        for (n = 48; n <= 57; n++)
-        {
-        putchar(n);
-        }
-        for (ch = 'a'; ch <= 'f'; ch++)
-        {
-        putchar(ch);
-        }
-        putchar(10);
-
-        return (0);
+return (0);
 }
