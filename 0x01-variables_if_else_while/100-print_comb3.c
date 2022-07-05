f64bb2fe0b88f3c6f7677260d7f28c2b363d3d22
Nat-251 <natygizachew@gmail.com>
2022-07-05 03:32:17 -0700

    hotfix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	int ch;
 	int n;
+
 	for (ch = 48; ch<= 57; ch++)
 	{
 		for (n = 49; n <= 57; n++)
@@ -19,7 +20,7 @@ int main(void)
 			{
 				putchar(ch);
 				putchar(n);
-				if (ch != 56 || n!= 57)
+				if (ch != 56 || n! = 57)
 				{
 					putchar(44);
 					putchar(32);
