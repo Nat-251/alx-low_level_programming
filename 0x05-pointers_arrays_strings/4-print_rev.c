dd38196ca235e6b6ae188db4e42ec28c0be717f1
Nat-251 <natygizachew@gmail.com>
2022-07-11 19:32:34 -0700

    C-0x05-3-4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,9 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	char *str;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
-	_putchar('\n');
+	str = "Holberton!";
+	print_rev(str);
+	return (0);
 }
