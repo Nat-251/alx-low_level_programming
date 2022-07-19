f81077592ca83f9066efbc254c129e689655b75c
Nat-251 <natygizachew@gmail.com>
2022-07-19 11:25:35 -0700

    minor fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
-
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+		*(dest + ) = *(src + i);
 
 	return (dest);
 }
