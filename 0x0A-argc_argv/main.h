700d3a967f1d9cd3e508b8062954d55158b66d22
Nat-251 <natygizachew@gmail.com>
2022-07-22 08:06:34 -0700

    C-0x0A-2

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,4 @@
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
+
