eb79dbc5e3cd6630f0c4461b18bb2e113ac58f84
Nat-251 <natygizachew@gmail.com>
2023-10-07 01:50:57 +0300

    loops-edit-1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase
- * @c: character to check
- * Return: 1 or 0
+ * _isupper - Checks for uppercase characters
+ * @c: The character to be checked
+ *
+ * Return: 1 if @c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	return ((c >= 65) && (c <= 90));
+	if (c > '@' && c < '[')
+		return (1);
+
+	return (0);
 }
